b989c2e8d1f836f0cf31b9ba7ada1cb32f0d2285
morgandevmh <morgan477.mh@gmail.com>
2026-02-04 11:43:24 +0100

    carrrection/express-version

---
+++ js/index2.js
@@ -0,0 +1,19 @@
+const express = require ('express')
+const path = require('path')
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req,res) => res.sendFile(path.join(__dirname, '..', 'html', 'index.html')))
+
+app.get("/about", (req,res) => res.sendFile(path.join(__dirname, '..', 'html', 'about.html')))
+
+app.get("/contact-me", (req,res) => res.sendFile(path.join(__dirname, '..', 'html', 'contact-me.html')))
+
+app.get("/404", (req,res) => res.sendFile(path.join(__dirname, '..', 'html', '404.html')))
+
+
+app.listen(PORT, (error) => {
+    if (error) throw error;
+    console.log(`Serveur lancé sur http://localhost:${PORT}`);
+  });
